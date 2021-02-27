a0e1180b6642c6ca3a0eaf1c05b4d8e814e5e810
Samin1615088 <samin.shafkat@gmail.com>
2021-02-28 00:21:10 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -2,34 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let person = {
-    name: 'Samin',
-    job: 'coder'
-  };
-
-  let pStyle = {
-    color: '#FC600A',
-    backgroundColor: '#F7E0D4',
-    borderRadius: "5px",
-    padding: "10px"
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p style={pStyle}>My React Paragraph. Created by {person.job} {person.name}</p>
+        
       </header>
     </div>
   );
